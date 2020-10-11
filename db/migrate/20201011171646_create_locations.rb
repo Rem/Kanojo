52ad1c7e9930690dc3df56353bfce4404b4ac90a
rem <chrisxcheong@gmail.com>
2020-10-11 13:30:52 -0400

    Update location table

--- db/migrate/20201011171646_create_locations.rb
+++ db/migrate/20201011171646_create_locations.rb
@@ -2,7 +2,7 @@ class CreateLocations < ActiveRecord::Migration[6.0]
   def change
     create_table :locations do |t|
       t.belongs_to :girlfriend, null: false, foreign_key: true
-      t.string :location
+      t.string :address
 
       t.timestamps
     end
